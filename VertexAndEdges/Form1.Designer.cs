2baa8d9e35c8bb7a07906490ab33ba43bc1143fa
jamesnogra <love.gerald@yahoo.com>
2017-06-18 15:00:07 +0800

    First Commit

---
+++ VertexAndEdges/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace VertexAndEdges
+{
+    partial class VertexAndEdges
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openGraphToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blindSearchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bFSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dFSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainPicBox = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.connectVertexButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toVertexComboBox = new System.Windows.Forms.ComboBox();
+            this.fromVertexComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.showAdjacencyMatrixButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPicBox)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.blindSearchToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(884, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.openGraphToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save Graph";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openGraphToolStripMenuItem
+            // 
+            this.openGraphToolStripMenuItem.Name = "openGraphToolStripMenuItem";
+            this.openGraphToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openGraphToolStripMenuItem.Text = "Open Graph";
+            this.openGraphToolStripMenuItem.Click += new System.EventHandler(this.openGraphToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // blindSearchToolStripMenuItem
+            // 
+            this.blindSearchToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bFSToolStripMenuItem,
+            this.dFSToolStripMenuItem});
+            this.blindSearchToolStripMenuItem.Name = "blindSearchToolStripMenuItem";
+            this.blindSearchToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.blindSearchToolStripMenuItem.Text = "Blind Search";
+            // 
+            // bFSToolStripMenuItem
+            // 
+            this.bFSToolStripMenuItem.Name = "bFSToolStripMenuItem";
+            this.bFSToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bFSToolStripMenuItem.Text = "Breadth-First Search";
+            // 
+            // dFSToolStripMenuItem
+            // 
+            this.dFSToolStripMenuItem.Name = "dFSToolStripMenuItem";
+            this.dFSToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dFSToolStripMenuItem.Text = "Depth-First Search";
+            // 
+            // mainPicBox
+            // 
+            this.mainPicBox.BackColor = System.Drawing.Color.White;
+            this.mainPicBox.Location = new System.Drawing.Point(12, 27);
+            this.mainPicBox.Name = "mainPicBox";
+            this.mainPicBox.Size = new System.Drawing.Size(690, 622);
+            this.mainPicBox.TabIndex = 1;
+            this.mainPicBox.TabStop = false;
+            this.mainPicBox.Click += new System.EventHandler(this.mainPicBox_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.connectVertexButton);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.toVertexComboBox);
+            this.groupBox1.Controls.Add(this.fromVertexComboBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(708, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(164, 112);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connect Vertex (Edge)";
+            // 
+            // connectVertexButton
+            // 
+            this.connectVertexButton.Location = new System.Drawing.Point(45, 79);
+            this.connectVertexButton.Name = "connectVertexButton";
+            this.connectVertexButton.Size = new System.Drawing.Size(75, 23);
+            this.connectVertexButton.TabIndex = 6;
+            this.connectVertexButton.Text = "Connect";
+            this.connectVertexButton.UseVisualStyleBackColor = true;
+            this.connectVertexButton.Click += new System.EventHandler(this.connectVertexButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "To:";
+            // 
+            // toVertexComboBox
+            // 
+            this.toVertexComboBox.FormattingEnabled = true;
+            this.toVertexComboBox.Location = new System.Drawing.Point(45, 52);
+            this.toVertexComboBox.Name = "toVertexComboBox";
+            this.toVertexComboBox.Size = new System.Drawing.Size(113, 21);
+            this.toVertexComboBox.TabIndex = 4;
+            // 
+            // fromVertexComboBox
+            // 
+            this.fromVertexComboBox.FormattingEnabled = true;
+            this.fromVertexComboBox.Location = new System.Drawing.Point(45, 24);
+            this.fromVertexComboBox.Name = "fromVertexComboBox";
+            this.fromVertexComboBox.Size = new System.Drawing.Size(113, 21);
+            this.fromVertexComboBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From:";
+            // 
+            // showAdjacencyMatrixButton
+            // 
+            this.showAdjacencyMatrixButton.Location = new System.Drawing.Point(709, 163);
+            this.showAdjacencyMatrixButton.Name = "showAdjacencyMatrixButton";
+            this.showAdjacencyMatrixButton.Size = new System.Drawing.Size(163, 23);
+            this.showAdjacencyMatrixButton.TabIndex = 3;
+            this.showAdjacencyMatrixButton.Text = "Show Adjacency Matrix";
+            this.showAdjacencyMatrixButton.UseVisualStyleBackColor = true;
+            this.showAdjacencyMatrixButton.Click += new System.EventHandler(this.showAdjacencyMatrixButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // VertexAndEdges
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 661);
+            this.Controls.Add(this.showAdjacencyMatrixButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.mainPicBox);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "VertexAndEdges";
+            this.Text = "Vertex and Edges";
+            this.Load += new System.EventHandler(this.VertexAndEdges_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPicBox)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blindSearchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bFSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dFSToolStripMenuItem;
+        private System.Windows.Forms.PictureBox mainPicBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox fromVertexComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox toVertexComboBox;
+        private System.Windows.Forms.Button connectVertexButton;
+        private System.Windows.Forms.Button showAdjacencyMatrixButton;
+        private System.Windows.Forms.ToolStripMenuItem openGraphToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ VertexAndEdges/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VertexAndEdges
+{
+
+    public partial class VertexAndEdges : Form
+    {
+        private Vertex[] vertices = new Vertex[256];
+        private int lastIndex = 0;
+        private string mainColor = "#000000";
+
+        public VertexAndEdges()
+        {
+            InitializeComponent();
+        }
+
+        private void VertexAndEdges_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void mainPicBox_Click(object sender, EventArgs e)
+        {
+            int mouseX, mouseY;
+            MouseEventArgs me = (MouseEventArgs)e;
+            mouseX = me.X;
+            mouseY = me.Y;
+            //MessageBox.Show("X is " + this.mouseX + " and Y is " + this.mouseY);
+            drawVertexAndLabel((lastIndex+1).ToString(), mouseX, mouseY);
+            updateFromAndToVertexComboBox();
+            saveToVertex(lastIndex, mouseX, mouseY);
+        }
+
+        private void saveToVertex(int indexNumber, int mouseX, int mouseY)
+        {
+            vertices[lastIndex] = new Vertex(indexNumber, mouseX, mouseY);
+            lastIndex++;
+        }
+
+        private void updateFromAndToVertexComboBox()
+        {
+            fromVertexComboBox.Items.Clear();
+            toVertexComboBox.Items.Clear();
+            for (int x=0; x<=lastIndex;x++)
+            {
+                fromVertexComboBox.Items.Add(x + 1);
+                toVertexComboBox.Items.Add(x + 1);
+            }
+        }
+
+        private void connectVertexButton_Click(object sender, EventArgs e)
+        {
+            int fromVertex=1, toVertex=1;
+            if (!Int32.TryParse(fromVertexComboBox.Text, out fromVertex) || !Int32.TryParse(toVertexComboBox.Text, out toVertex))
+            {
+                MessageBox.Show("Invalid vertex selected.");
+                return;
+            }
+            //MessageBox.Show("Connect from " + vertices[fromVertex - 1].indexNumber + " to " + vertices[toVertex - 1].indexNumber);
+
+            //connect the vertices in the variable
+            if (updateNeighbors(fromVertex, toVertex))
+            {
+                drawEdgeLineAndRelabel(
+                    (vertices[fromVertex - 1].indexNumber + 1).ToString(),
+                    (vertices[toVertex - 1].indexNumber + 1).ToString(),
+                    vertices[fromVertex - 1].mouseX,
+                    vertices[fromVertex - 1].mouseY,
+                    vertices[toVertex - 1].mouseX,
+                    vertices[toVertex - 1].mouseY
+                );
+            }
+        }
+
+        public void drawVertexAndLabel(string label, int posX, int posY)
+        {
+            //draw the circle vertex
+            Bitmap bmp = new Bitmap(mainPicBox.Width, mainPicBox.Height);
+            Graphics g = mainPicBox.CreateGraphics();
+            Brush blackPen = new SolidBrush(ColorTranslator.FromHtml(mainColor));
+            Font myFont = new Font("Arial", 12);
+            g.FillEllipse(blackPen, posX - 20, posY - 20, 40, 40);
+            g.DrawString(label, myFont, Brushes.White, new Point(posX - 9, posY - 8));
+        }
+
+        public void drawEdgeLineAndRelabel(string label1, string label2, int posX1, int posY1, int posX2, int posY2)
+        {
+            //then draw the line
+            Bitmap bmp = new Bitmap(mainPicBox.Width, mainPicBox.Height);
+            Graphics g = mainPicBox.CreateGraphics();
+            Pen blackPen = new Pen(ColorTranslator.FromHtml(mainColor), 3);
+            g.DrawLine(blackPen, posX1, posY1, posX2, posY2);
+
+            //draw the label again because the line might have overlapped it
+            Font myFont = new Font("Arial", 12);
+            g.DrawString(label1, myFont, Brushes.White, new Point(posX1 - 9, posY1 - 8));
+            g.DrawString(label2, myFont, Brushes.White, new Point(posX2 - 9, posY2 - 8));
+        }
+
+        private bool updateNeighbors(int fromVertex, int toVertex)
+        {
+            try
+            {
+                //iterate first through the List if they are already neighbors
+                if (!checkIfListContains(vertices[fromVertex - 1].neighbors, toVertex - 1))
+                {
+                    vertices[fromVertex - 1].neighbors.Add(toVertex - 1);
+                    vertices[toVertex - 1].neighbors.Add(fromVertex - 1);
+                }
+                else
+                {
+                    MessageBox.Show("That edge from " + fromVertex + " to " + toVertex + " already exists.");
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("That edge from " + fromVertex + " to " + toVertex + " does not exist.");
+                return false;
+            }
+            return true;
+        }
+
+        private bool checkIfListContains(List<int> tempList, int key)
+        {
+            foreach (int x in tempList)
+            {
+                if (x == key)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void showAdjacencyMatrixButton_Click(object sender, EventArgs e)
+        {
+            string tempText = "\t";
+            for (int z = 0; z < lastIndex; z++)
+            {
+                tempText += (vertices[z].indexNumber + 1) + "\t";
+            }
+            tempText = tempText.Remove(tempText.Length-1);
+            for (int x = 0; x < lastIndex; x++)
+            {
+                tempText += "\n\n"+(x+1);
+                for (int y = 0; y < lastIndex; y++)
+                {
+                    if (x == y)
+                    {
+                        tempText += "\t0";
+                    }
+                    else
+                    {
+                        if (checkIfListContains(vertices[x].neighbors, y))
+                        {
+                            tempText += "\t1";
+                        }
+                        else if (checkIfListContains(vertices[y].neighbors, x))
+                        {
+                            tempText += "\t1";
+                        }
+                        else
+                        {
+                            tempText += "\t0";
+                        }
+                    }
+                }
+            }
+            MessageBox.Show(tempText, "Adjacency Matrix");
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var tempText = "";
+            for (int x=0; x < lastIndex; x++)
+            {
+                tempText += vertices[x].indexNumber + " ";
+                tempText += vertices[x].mouseX + " " + vertices[x].mouseY + " ";
+                foreach (int neighbor in vertices[x].neighbors)
+                {
+                    tempText += neighbor + " ";
+                }
+                tempText = tempText.Remove(tempText.Length - 1);
+                tempText += "\n";
+            }
+            tempText = tempText.Remove(tempText.Length - 1);
+            //MessageBox.Show(tempText);
+
+            //then save to text file
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.FileName = "TempGraph.txt";
+            saveFile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFile.FileName, tempText);
+            }
+        }
+
+        private void openGraphToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string readText = "";
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string filePath = openFileDialog1.FileName;
+                try
+                {
+                    readText = File.ReadAllText(filePath);
+                    insertAllVerticesAndEdges(readText);
+                }
+                catch (IOException) { }
+            }
+            //MessageBox.Show(readText);
+        }
+
+        private void insertAllVerticesAndEdges(string tempString)
+        {
+            lastIndex = 0; //reset the index before reinserting all vertices and edges
+            string[] verticesLine = tempString.Split('\n');
+            for (int x = 0; x < verticesLine.Length; x++)
+            {
+                string[] allElements = verticesLine[x].Split(' ');
+                try
+                {
+                    vertices[x] = new Vertex(Int32.Parse(allElements[0]), Int32.Parse(allElements[1]), Int32.Parse(allElements[2]));
+                    //reinsert the neighbors of this vertex
+                    for (int y = 3; y < allElements.Length; y++)
+                    {
+                        vertices[x].neighbors.Add(Int32.Parse(allElements[y]));
+                    }
+                    updateFromAndToVertexComboBox();
+                    lastIndex++;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Something is wrong with that file.");
+                }
+                
+            }
+
+            //clear the picturebox
+            Bitmap bmp = new Bitmap(mainPicBox.Width, mainPicBox.Height);
+            Graphics g = mainPicBox.CreateGraphics();
+            Brush blackPen = new SolidBrush(ColorTranslator.FromHtml("#FFFFFF"));
+            g.FillRectangle(blackPen, 0, 0, mainPicBox.Width, mainPicBox.Height);
+            //then lets draw the vertices and edges
+            for (int x = 0; x < lastIndex; x++)
+            {
+                //MessageBox.Show("Drawing vertex " + vertices[x].indexNumber.ToString());
+                drawVertexAndLabel((vertices[x].indexNumber+1).ToString(), vertices[x].mouseX, vertices[x].mouseY);
+                foreach (int neighbor in vertices[x].neighbors)
+                {
+                    drawEdgeLineAndRelabel(
+                        (vertices[x].indexNumber+1).ToString(),
+                        (vertices[neighbor].indexNumber+1).ToString(),
+                        vertices[x].mouseX,
+                        vertices[x].mouseY,
+                        vertices[neighbor].mouseX,
+                        vertices[neighbor].mouseY
+                    );
+                }
+            }
+        }
+    }
+
+    public class Vertex
+    {
+        public int indexNumber;
+        public int mouseX;
+        public int mouseY;
+        public List<int> neighbors;
+
+        public Vertex(int indexNumber, int mouseX, int mouseY)
+        {
+            this.indexNumber = indexNumber;
+            this.mouseX = mouseX;
+            this.mouseY = mouseY;
+            neighbors = new List<int>();
+        }
+
+        public override string ToString()
+        {
+            string tempString = "Index: "+this.indexNumber;
+            tempString += "\nPos: "+this.mouseX+","+this.mouseY;
+            tempString += "\nNeighbors: ";
+            foreach (int a in this.neighbors)
+            {
+                tempString += a.ToString() + " ";
+            }
+            return tempString;
+        }
+    }
+}
